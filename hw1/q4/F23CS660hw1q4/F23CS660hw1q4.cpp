ec75c20095751cc6b2ab479a32ab965bb0f9de02
Rebecca Schiele <rschiele9807@sdsu.edu>
2023-09-10 13:17:53 -0700

    First attempt at establishing the recursion cases.

--- hw1/q4/F23CS660hw1q4/F23CS660hw1q4.cpp
+++ hw1/q4/F23CS660hw1q4/F23CS660hw1q4.cpp
@@ -2,37 +2,85 @@
 //
 //References: https://www.geeksforgeeks.org/find-maximum-path-sum-in-a-binary-tree/
 //https://leetcode.com/problems/binary-tree-maximum-path-sum/
+//https://www.geeksforgeeks.org/stdmax-in-cpp/
 
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
+// A binary tree vertex
 struct vertex
 {
-    int number;
-	struct vertex *left, *right;
+	int number;
+	struct vertex* left, * right;
 };
 
+// A utility function to allocate a new vertex
 struct vertex* newVertex(int number)
 {
-    struct vertex* newVertex = new vertex;
-    newVertex->number = number;
-    newVertex->left = newVertex->right = NULL;
-    return (newVertex);
+	struct vertex* newVertex = new vertex;
+	newVertex->number = number;
+	newVertex->left = newVertex->right = NULL;
+	return (newVertex);
 }
 
 // This function returns overall maximum path sum in 'res'
-// And returns max path sum going through root.
-int MaxSumPath(vertex* v)
+// And returns max path sum going through vertex v.
+int MaxSumPath(vertex* v, int& res)
 {
-    // Base Case
-    if (v == NULL)
-        return 0;
+	int currentRes = res;
+	int childSumPath = INT_MIN;
+	int currentMax = INT_MIN;
+	int rootSumPath = MaxSumPath(v, res);
+
+	// Base Case
+	if (v == NULL)
+		return 0;
+
+	//Recursive Cases
+
+	if (v->left == NULL && v->right == NULL)
+	{
+		currentMax = rootSumPath;
+	}
+
+	if (v->left == NULL && v->right != NULL)
+	{
+		
+		childSumPath = MaxSumPath(v->right, res);
+		currentMax = max(childSumPath, rootSumPath);
+	}
+
+	if (v->left != NULL && v->right == NULL)
+	{
+		childSumPath = MaxSumPath(v->left, res);
+		currentMax = max(childSumPath, rootSumPath);
+	}
+
+	if (v->left != NULL && v->right != NULL)
+	{
+		childSumPath = max(MaxSumPath(v->left, res), MaxSumPath(v->right, res));
+		currentMax = max(childSumPath, rootSumPath);
+	}
+
+	res = (currentRes, currentMax);
 }
 
+// Returns maximum path sum in tree with given root
+int findMaxSum(vertex* v)
+{
+	// Initialize result
+	int res = INT_MIN;
+
+	// Compute and return result
+	MaxSumPath(v, res);
+	return res;
+}
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
